3b87115924a08a1bbcad3945632570edd49dff6c
Will Lytle <wlytle89@gmail.com>
2020-08-13 17:00:54 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,5 +1,9 @@
 def find_element_index(array, value_to_find)
-  for i in array do
-    
+  i = 0
+  while i < array.length do
+    if array[i] == value_to_find
+      return i
+    end
+    i +=1
   end
 end
